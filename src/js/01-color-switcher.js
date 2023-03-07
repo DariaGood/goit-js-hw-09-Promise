5e15330a2eb81f7fc1f78a86f14415c757de317e
DariaGood <102043020+DariaGood@users.noreply.github.com>
2023-03-07 21:57:34 +0200

    2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const body = document.getElementsByTagName('body')[0];
+let timerId = null;
 
+btnStart.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  btnStart.setAttribute('disabled', true);
+  btnStop.removeAttribute('disabled');
+});
+
+btnStop.addEventListener('click', () => {
+  clearInterval(timerId);
+  
+  btnStart.removeAttribute('disabled');
+  btnStop.setAttribute('disabled', true);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
